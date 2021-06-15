c8da1fd0189a9e1a8bf06bc2a11456f37ef60b72
Tela Caul <telacaul@gmail.com>
2021-06-15 12:35:56 -0700

    renamed directory

--- config/connection.js
+++ config/connection.js
@@ -1,19 +1,19 @@
+//dependencies
+//sequelize constructor
 const Sequelize = require('sequelize');
-
+//dotenv for local environmental variables: username and password
 require('dotenv').config();
 
-const sequelize = process.env.JAWSDB_URL
-  ? new Sequelize(process.env.JAWSDB_URL)
-  : new Sequelize('sequelize_library', 'root', '', {
-      host: 'localhost',
-      port: 3306,
-      dialect: 'mysql',
-      pool: {
-        max: 5,
-        min: 0,
-        idle: 10000
-      }
-    });
+let sequelize;
 
-    
+if (process.env.JAWSDB_URL) {
+  sequelize = new Sequelize(process.env.JAWSDB_URL);
+} else {
+  sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
+    host: 'localhost',
+    dialect: 'mysql',
+    port: 3306
+  });
+}
+
 module.exports = sequelize;
